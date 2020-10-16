17d863a12707fef7a170625356fbbcc19e334c00
Kimo Silva <e.k.silva441@gmail.com>
2020-10-16 14:54:26 -0700

    framework for create ride page

--- client/src/components/CreateRide.jsx
+++ client/src/components/CreateRide.jsx
@@ -5,15 +5,36 @@ class CreateRide extends React.Component {
     super(props);
   
     this.state = {
-       
+      destination: '',
+      departure: '',
+      seats: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
-  
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <form>
+        <label htmlFor='destination' >Destination</label>
+        <input type='' id='destination' onChange={this.handleChange} value={this.state.destination} />
+
+        <label htmlFor='departure' >Departure</label>
+        <input type='' id='departure' onChange={this.handleChange} value={this.state.departure} />
+
+        <label htmlFor='seats' >Free Seats</label>
+        <input type='' id='seats' onChange={this.handleChange} value={this.state.seats} />
+
+        <div>
+          <button type='button' onClick={this.clearForm}>Clear</button>
+          <button type='submit'>Send Invite</button>
+        </div>
+      </form>
     )
   }
 }
